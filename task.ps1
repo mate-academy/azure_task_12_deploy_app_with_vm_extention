87fbc2d731e4f665aab5a3ae7b5ac860ef042ad7
Iryna Lyakhova <ippk0629@gmail.com>
2024-06-08 19:30:59 +0300

    Error

--- task.ps1
+++ task.ps1
@@ -9,7 +9,7 @@ $sshKeyName = "linuxboxsshkey"
 $sshKeyPublicKey = Get-Content "C:\Users\ipppk\.ssh\id_rsa.pub" -Raw
 $publicIpAddressName = "linuxboxpip"
 $vmName = "matebox"
-$vmImage = "Canonical:UbuntuServer:18.04-LTS:latest"
+$vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
 $dnsLabel = "matetask" + (Get-Random -Count 1)
 
@@ -17,64 +17,45 @@ Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
 Write-Host "Creating a network security group $networkSecurityGroupName ..."
-$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow
-$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow
+$nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow;
+$nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
 New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
+Write-Host "Creating a virtual network ..."
 $subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
 New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet
 
+Write-Host "Creating a SSH key ..."
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
+Write-Host "Creating a Public IP Address ..."
 New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel $dnsLabel
 
-Write-Host "Creating network interface..."
-$nic = New-AzNetworkInterface -ResourceGroupName $resourceGroupName -Location $location -Name "${vmName}Nic" `
-    -SubnetId (Get-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName).Subnets[0].Id `
-    -PublicIpAddressId (Get-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName).Id `
-    -NetworkSecurityGroupId (Get-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName).Id
+Write-Host "Creating a VM ..."
+New-AzVm `
+-ResourceGroupName $resourceGroupName `
+-Name $vmName `
+-Location $location `
+-image $vmImage `
+-size $vmSize `
+-SubnetName $subnetName `
+-VirtualNetworkName $virtualNetworkName `
+-SecurityGroupName $networkSecurityGroupName `
+-SshKeyName $sshKeyName  -PublicIpAddressName $publicIpAddressName
 
-Write-Host "Creating VM $vmName ..."
-$cred = New-Object System.Management.Automation.PSCredential ("azureuser", (ConvertTo-SecureString "placeholderpassword" -AsPlainText -Force))
-$vmConfig = New-AzVmConfig -VMName $vmName -VMSize $vmSize | `
-    Set-AzVMOperatingSystem -Linux -ComputerName $vmName -Credential $cred -DisablePasswordAuthentication | `
-    Set-AzVMSourceImage -PublisherName "Canonical" -Offer "0001-com-ubuntu-server-jammy" -Skus "22_04-lts-gen2" -Version "latest" | `
-    Add-AzVMNetworkInterface -Id $nic.Id | `
-    Add-AzVMSSHPublicKey -KeyData $sshKeyPublicKey -Path "/home/azureuser/.ssh/authorized_keys"
-
-Write-Host "Starting VM creation..."
-$vm = New-AzVm -ResourceGroupName $resourceGroupName -Location $location -VM $vmConfig
-
-Write-Host "Waiting for VM to be fully provisioned..."
-Start-Sleep -Seconds 60
-
-# Перевірка стану VM
-$vmState = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName
-if ($vmState.ProvisioningState -ne 'Succeeded') {
-    Write-Host "VM is not in 'Succeeded' state. Current state: $($vmState.ProvisioningState)"
-    exit
-}
-
-Write-Host "Adding CustomScript extension to VM..."
-$scriptUrl = "https://raw.githubusercontent.com/ILyakhova/azure_task_12_deploy_app_with_vm_extention/develop/install-app.sh"
-$publicSettings = @{
-    "fileUris" = @($scriptUrl)
+$extensionName = "installAppExtension"
+$scriptUri = "https://raw.githubusercontent.com/ILyakhova/azure_task_12_deploy_app_with_vm_extention/develop/install-app.sh"
+Set-AzVMExtension `
+-ResourceGroupName $resourceGroupName `
+-VMName $vmName `
+-Name $extensionName `
+-ExtensionType "CustomScript" `
+-Publisher "Microsoft.Azure.Extensions" `
+-TypeHandlerVersion "2.0" `
+-Settings @{
+    "fileUris" = @($scriptUri)
     "commandToExecute" = "sh install-app.sh"
 }
 
-try {
-    Set-AzVMExtension `
-    -ResourceGroupName $resourceGroupName `
-    -VMName $vmName `
-    -Name "CustomScriptExtension" `
-    -Publisher "Microsoft.Azure.Extensions" `
-    -ExtensionType "CustomScript" `
-    -TypeHandlerVersion "2.0" `
-    -SettingString (ConvertTo-Json $publicSettings)
-    Write-Host "CustomScript extension added successfully."
-} catch {
-    Write-Host "Error adding CustomScript extension: $_"
-    exit
-}
 
 Write-Host "Deployment complete. You can access your app at http://$($dnsLabel).uksouth.cloudapp.azure.com:8080"
