65f201a129227ee3db2930994af32159350af6f2
KempachiDZ <alexmelnyk48370@gmail.com>
2025-06-30 18:48:50 +0300

    solve task

--- task.ps1
+++ task.ps1
@@ -1,4 +1,4 @@
-$location = "uksouth"
+$location = "westeurope"
 $resourceGroupName = "mate-azure-task-12"
 $networkSecurityGroupName = "defaultnsg"
 $virtualNetworkName = "vnet"
@@ -11,23 +11,27 @@ $publicIpAddressName = "linuxboxpip"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
-$dnsLabel = "matetask" + (Get-Random -Count 1) 
+$dnsLabel = "matetask" + (Get-Random -Count 1)
 
-Write-Host "Creating a resource group $resourceGroupName ..."
+Write-Host "Створення групи ресурсів $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
 
-Write-Host "Creating a network security group $networkSecurityGroupName ..."
+Write-Host "Створення мережевої групи безпеки $networkSecurityGroupName ..."
 $nsgRuleSSH = New-AzNetworkSecurityRuleConfig -Name SSH  -Protocol Tcp -Direction Inbound -Priority 1001 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 22 -Access Allow;
 $nsgRuleHTTP = New-AzNetworkSecurityRuleConfig -Name HTTP  -Protocol Tcp -Direction Inbound -Priority 1002 -SourceAddressPrefix * -SourcePortRange * -DestinationAddressPrefix * -DestinationPortRange 8080 -Access Allow;
 New-AzNetworkSecurityGroup -Name $networkSecurityGroupName -ResourceGroupName $resourceGroupName -Location $location -SecurityRules $nsgRuleSSH, $nsgRuleHTTP
 
+Write-Host "Створення віртуальної мережі $virtualNetworkName ..."
 $subnet = New-AzVirtualNetworkSubnetConfig -Name $subnetName -AddressPrefix $subnetAddressPrefix
 New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroupName -Location $location -AddressPrefix $vnetAddressPrefix -Subnet $subnet
 
+Write-Host "Створення SSH ключа $sshKeyName ..."
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
-New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Basic -AllocationMethod Dynamic -DomainNameLabel $dnsLabel
+Write-Host "Створення публічної IP адреси $publicIpAddressName ..."
+New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $dnsLabel
 
+Write-Host "Створення віртуальної машини $vmName ..."
 New-AzVm `
 -ResourceGroupName $resourceGroupName `
 -Name $vmName `
@@ -39,4 +43,43 @@ New-AzVm `
 -SecurityGroupName $networkSecurityGroupName `
 -SshKeyName $sshKeyName  -PublicIpAddressName $publicIpAddressName
 
-# ↓↓↓ Write your code here ↓↓↓
+# ↓↓↓ Код для встановлення розширення Custom Script Extension ↓↓↓
+Start-Sleep -Seconds 20
+
+Write-Host "Перевірка створення VM..."
+$vm = Get-AzVM -ResourceGroupName $resourceGroupName -Name $vmName -ErrorAction SilentlyContinue
+
+if ($vm) {
+    Write-Host "VM створено успішно. Встановлення Custom Script Extension..."
+
+
+    $githubUsername = "Alexionx"
+    $scriptUri = "https://raw.githubusercontent.com/$githubUsername/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh"
+
+    Set-AzVMExtension `
+        -ResourceGroupName $resourceGroupName `
+        -VMName $vmName `
+        -Name "CustomScript" `
+        -Publisher "Microsoft.Azure.Extensions" `
+        -ExtensionType "CustomScript" `
+        -TypeHandlerVersion "2.1" `
+        -Settings @{
+            "fileUris" = @($scriptUri)
+            "commandToExecute" = "bash install-app.sh"
+        } `
+        -Location $location
+
+    Write-Host "Отримання інформації про публічну IP адресу..."
+    $publicIp = Get-AzPublicIpAddress -ResourceGroupName $resourceGroupName -Name $publicIpAddressName
+    $ipAddress = $publicIp.IpAddress
+    $dnsName = $publicIp.DnsSettings.Fqdn
+
+    Write-Host "Розгортання завершено!"
+    Write-Host "IP адреса: $ipAddress"
+    Write-Host "DNS ім'я: $dnsName"
+    Write-Host "Веб-додаток буде доступний за адресою: http://$dnsName:8080"
+    Write-Host "Або за IP: http://$ipAddress:8080"
+    Write-Host "Зачекайте кілька хвилин, поки встановиться додаток, потім перевірте URL у браузері."
+} else {
+    Write-Host "❌ Помилка: VM не було створено. Перевірте помилки вище."
+}
