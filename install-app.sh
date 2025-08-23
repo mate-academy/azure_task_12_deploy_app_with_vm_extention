d26128c031c2a6f9a0e2b8e1c7c7eb524287569f
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-23 20:40:14 +0300

    added tralling spash in cp command in install-app.sh

--- install-app.sh
+++ install-app.sh
@@ -14,7 +14,7 @@ apt-get install python3-pip -yq
 mkdir /app 
 # make sure to uncomment the line bellow and update the link with your GitHub username
 git clone https://github.com/zave52/azure_task_12_deploy_app_with_vm_extention.git
-cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
+cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app/
 
 # create a service for the app via systemctl and start the app
 mv /app/todoapp.service /etc/systemd/system/
