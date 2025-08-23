5b54b156099d405bdd6da8eaabb0f6678df78bb1
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-23 21:58:28 +0300

    changed protected settings to setting in params for az vm custom script extension in task.ps1

--- task.ps1
+++ task.ps1
@@ -48,7 +48,7 @@ $Params = @{
     Publisher          = "Microsoft.Azure.Extensions"
     ExtensionType      = "CustomScript"
     TypeHandlerVersion = "2.1"
-    ProtectedSettings  = @{
+    Settings  = @{
         fileUris = @("https://raw.githubusercontent.com/zave52/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh");
         commandToExecute = "./install-app.sh"
     }
