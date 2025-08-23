d36c3b9ff9b0f700b2c475686516f7de012d0f7a
zakhar <zakhar.savchyn.dev@gmail.com>
2025-08-23 23:01:49 +0300

    refactored way of creating vm and related resources in task.ps1

--- task.ps1
+++ task.ps1
@@ -11,7 +11,7 @@ $publicIpAddressName = "linuxboxpip"
 $vmName = "matebox"
 $vmImage = "Ubuntu2204"
 $vmSize = "Standard_B1s"
-$dnsLabel = "matetask" + (Get-Random -Count 1) 
+$vmCount = 1
 
 Write-Host "Creating a resource group $resourceGroupName ..."
 New-AzResourceGroup -Name $resourceGroupName -Location $location
@@ -26,31 +26,39 @@ New-AzVirtualNetwork -Name $virtualNetworkName -ResourceGroupName $resourceGroup
 
 New-AzSshKey -Name $sshKeyName -ResourceGroupName $resourceGroupName -PublicKey $sshKeyPublicKey
 
-New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $dnsLabel
+for ($i = 1; $i -le $vmCount; $i++) {
+    $vmName = "matebox-$i"
+    $publicIpAddressName = "linuxboxpip-$i"
+    $dnsLabel = "matetask$i" + (Get-Random -Count 1)
 
-New-AzVm `
--ResourceGroupName $resourceGroupName `
--Name $vmName `
--Location $location `
--image $vmImage `
--size $vmSize `
--SubnetName $subnetName `
--VirtualNetworkName $virtualNetworkName `
--SecurityGroupName $networkSecurityGroupName `
--SshKeyName $sshKeyName  `
--PublicIpAddressName $publicIpAddressName
+    Write-Host "Creating VM $i of $vmCount : $vmName ..."
 
-Write-Host "Applying Azure VM Extension ..."
-$Params = @{
-    ResourceGroupName  = $resourceGroupName
-    VMName             = $vmName
-    Name               = "CustomScript"
-    Publisher          = "Microsoft.Azure.Extensions"
-    ExtensionType      = "CustomScript"
-    TypeHandlerVersion = "2.1"
-    Settings  = @{
-        fileUris = @("https://raw.githubusercontent.com/zave52/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh");
-        commandToExecute = "./install-app.sh"
+    New-AzPublicIpAddress -Name $publicIpAddressName -ResourceGroupName $resourceGroupName -Location $location -Sku Standard -AllocationMethod Static -DomainNameLabel $dnsLabel
+
+    New-AzVm `
+    -ResourceGroupName $resourceGroupName `
+    -Name $vmName `
+    -Location $location `
+    -image $vmImage `
+    -size $vmSize `
+    -SubnetName $subnetName `
+    -VirtualNetworkName $virtualNetworkName `
+    -SecurityGroupName $networkSecurityGroupName `
+    -SshKeyName $sshKeyName  `
+    -PublicIpAddressName $publicIpAddressName
+
+    Write-Host "Applying Azure VM Extension to $vmName ..."
+    $Params = @{
+        ResourceGroupName  = $resourceGroupName
+        VMName             = $vmName
+        Name               = "CustomScript"
+        Publisher          = "Microsoft.Azure.Extensions"
+        ExtensionType      = "CustomScript"
+        TypeHandlerVersion = "2.1"
+        Settings  = @{
+            fileUris = @("https://raw.githubusercontent.com/zave52/azure_task_12_deploy_app_with_vm_extention/main/install-app.sh");
+            commandToExecute = "./install-app.sh"
+        }
     }
+    Set-AzVMExtension @Params
 }
-Set-AzVMExtension @Params
