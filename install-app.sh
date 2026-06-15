3aca71b5779997d3f48ef6037f3d48e3efe6cf4a
Oleg <oleggavelya@gmail.com>
2026-06-15 21:26:09 +0300

    add install Library requirements.txt

--- install-app.sh
+++ install-app.sh
@@ -10,11 +10,13 @@
 apt-get update -yq
 apt-get install python3-pip -yq
 
-# Create a directory for the app and download the files. 
+
+# Create a directory for the app and download the files.
 mkdir /app 
 # make sure to uncomment the line bellow and update the link with your GitHub username
 git clone https://github.com/OlehHavelia1994/azure_task_12_deploy_app_with_vm_extention.git
 cp -r azure_task_12_deploy_app_with_vm_extention/app/* /app
+pip3 install -r /app/requirements.txt
 
 # create a service for the app via systemctl and start the app
 mv /app/todoapp.service /etc/systemd/system/
